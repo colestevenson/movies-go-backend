4d5b2a8f4415b4b7a96195974c2085f472b2992a
colestevenson <cole@newmoyergeospatial.com>
2022-04-11 16:34:02 -0400

    Lectures 35-47 - creating an api backend with routes, handlers, etc. Serves basic json from a postgres database called go_movies. This section was all in go, so no react changes here

---
+++ models/models.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+// wrapper for database
+type Models struct {
+	DB DBModel
+}
+
+// returns models with db pool
+func NewModels(db *sql.DB) Models {
+	return Models{
+		DB: DBModel{DB: db},
+	}
+}
+
+// type for movies table in database
+type Movie struct {
+	ID          int            `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Year        int            `json:"year"`
+	ReleaseDate time.Time      `json:"release_date"`
+	Runtime     int            `json:"runtime"`
+	Rating      int            `json:"rating"`
+	MPAARating  string         `json:"mpaa_rating"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt    time.Time      `json:"-"`
+	MovieGenre  map[int]string `json:"genres"`
+}
+
+// type for genres table in database
+type Genre struct {
+	ID        int       `json:"-"`
+	GenreName string    `json:"genre_name"`
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
+
+// type for moviegenre table in database
+type MovieGenre struct {
+	ID        int       `json:"-"`
+	MovieID   int       `json:"-"`
+	GenreID   int       `json:"-"`
+	Genre     Genre     `json:"genre"`
+	CreatedAt time.Time `json:"-"`
+	UpdateAt  time.Time `json:"-"`
+}
